6fa728fa551a6740e27611c72cc6b6037bd6dd30
Michael Barreto <Michael.barreto210@gmail.com>
2025-01-22 17:50:40 -0600

    initial commit for redo of project

---
+++ Website-Portfolio/src/Header.jsx
@@ -0,0 +1,23 @@
+function Header(){
+    
+    return(
+        <header>
+            <nav>
+                <ul className="headerBar">
+                    <li className="myName">
+                        <p>Michael Barreto</p>
+                    </li>
+
+                    <li className="headerTitle">About Me</li>
+
+                    <li className="headerTitle">Skills</li>
+
+                    <li className="headerTitle">Projects</li>
+                    
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ Website-Portfolio/src/LandingPage.jsx
@@ -0,0 +1,7 @@
+function LandingPage(){
+    return(
+        <>
+        { /* Work on the body next. Look up bro code's tutorial on how to add images to the website. */}
+        </>
+    );
+}
