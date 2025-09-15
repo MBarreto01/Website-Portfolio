fe000199d3dd7932117e675a8ef172b3c786689e
Michael Barreto <Michael.barreto210@gmail.com>
2025-09-15 16:48:32 -0500

    updated project cards to include stream pilot project, instead of contact book. Also updated VidScorePro section to be more explanatory.

--- Website-Portfolio/src/Projects.jsx
+++ Website-Portfolio/src/Projects.jsx
@@ -9,15 +9,18 @@ function Projects() {
             </p>
 
             <div className="projectsBody">
-                <Card title="VidScorePro" desc="Web application that analyzes a video, and gives the video a 
-                                                grade based off of various metrics."/>
+                <Card title="VidScorePro" desc="Collaborated on VidScore Pro, a web app that analyzes 
+                user-submitted videos and assigns grades based on predefined metrics. Designed and 
+                implemented a React frontend with JavaScript, HTML, and CSS for a seamless user experience. 
+                Focused on creating a visually appealing and intuitive interface for users."/>
 
                 <Card title="Website Portfolio" desc="Front-end React web application using JSX and CSS 
                             to create a personal portfolio website."/>
 
-                <Card title="Contact Book" desc="Stores employee information within a contact book for 
-                                                company records. Utilizes various Java classes, extensions, and 
-                                                arrays."/>
+                <Card title="Stream Pilot" desc="Built a full-stack app that automates YouTube live stream 
+                setup for LeetCode streamers. The React frontend offers one-click generation of problems, 
+                thumbnails, titles, and descriptions. A Python backend integrates OpenAI, LeetCode, and 
+                YouTube APIs to handle scheduling and workflow automation."/>
             </div>
             
         </div>
